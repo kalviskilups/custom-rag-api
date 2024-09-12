c0edb282e836287f666b5d68a2427204d0d4af99
kalvis.kilups <kalvis.kilups@edi.lv>
2024-09-12 23:11:46 +0300

    Improve responses and restructure for readability

--- client.py
+++ client.py
@@ -1,34 +1,48 @@
 import requests
 
-def ask_question():
+# Seit vajag noformet un server.py ari. kaut ka uztaisit profesionalak kodu un pamainit hiperparametrus. Also uzrakstit README.md.
+
+
+def ask_question() -> bool:
+    """
+    Prompt the user for a query, send it to the server, and display the response.
+
+    :return: A boolean indicating whether the program should continue or exit.
+    """
+
     query = input("Please enter your query (or type 'q' to quit): ")
-    
-    if query.lower() == 'q':
+
+    if query.lower() == "q":
         print("Exiting the program...")
         return False
 
-    response = requests.post(
-        "http://127.0.0.1:2026/query",
-        json={"query": query}
-    )
-    
+    response = requests.post("http://127.0.0.1:2026/query", json={"query": query})
+
     if response.status_code == 200:
         data = response.json()
         print("\nSources:")
         for source in data.get("sources", []):
-            print(f"\nSource {source['source']}, Page {source['page']}\n")
+            print(
+                f"\nSource {source['source']}, Page {source['page']}, Similarity {source['score']}\n"
+            )
             print(f"{source['content']}")
 
         print(data.get("answer", "No answer found"))
     else:
         print("Error:", response.json().get("error"))
-    
-    return True 
+
+    return True
+
 
 def main():
+    """
+    Main function to continuously prompt the user for queries until they choose to exit.
+    """
+
     keep_asking = True
     while keep_asking:
         keep_asking = ask_question()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,25 +1,155 @@
-from typing import List
-from langchain_core.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, PromptTemplate
+import re
+import torch
+import transformers
+from typing import List, Tuple
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    PromptTemplate,
+)
+from langchain_core.documents import Document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
-def custom_prompt_processing(template_str):
+
+def custom_prompt_processing(template_str: str) -> ChatPromptTemplate:
+    """
+    Create a custom prompt template for a language model using the provided template string.
+
+    :param template_str: The template string to use for generating the prompt.
+    :return: An instance of `ChatPromptTemplate` configured with the provided template.
+    """
 
     prompt_template = PromptTemplate(
-        input_variables=["context", "question"],
-        template=template_str
+        input_variables=["context", "question"], template=template_str
     )
 
     human_message_prompt = HumanMessagePromptTemplate(prompt=prompt_template)
 
-    # Create the ChatPromptTemplate instance
     prompt = ChatPromptTemplate(
-        input_variables=["context", "question"],
-        messages=[human_message_prompt]
-        )
+        input_variables=["context", "question"], messages=[human_message_prompt]
+    )
 
     return prompt
 
-def extract_answer(response_text):
+
+def extract_answer(response_text: str) -> str:
+    """
+    Extract the answer from the response text by splitting and cleaning it.
+
+    :param response_text: The text response from the language model that includes the answer.
+    :return: A string containing the cleaned answer. If no answer is found, returns "No answer found."
+    """
+
     parts = response_text.split("Answer:")
     if len(parts) > 1:
-        return f"\n\nAnswer: {parts[1].strip()}\n\n"
+        output = parts[1].strip()
+        cleaned_response = re.split(
+            r"(Question:|Context:|You are an assistant|AI:|Robot:|Human:)", output
+        )[0].strip()
+        return f"\n\nAnswer: {cleaned_response}\n\n"
     return "No answer found."
+
+
+def handle_missing_env_vars(variable: str, name: str) -> None:
+    """
+    Handle missing environment variables.
+
+    :param variable: The environment variable to check.
+    :param name: Name of the variable for the error message.
+    """
+
+    if variable is None:
+        raise ValueError(f"{name} not found. Please set it in the .env file.")
+
+
+def load_model_tokenizer_pipeline(model: str, token: str) -> Tuple[
+    transformers.PreTrainedTokenizer,
+    transformers.PreTrainedModel,
+    transformers.Pipeline,
+]:
+    """
+    Load a model, tokenizer, and text-generation pipeline from Hugging Face.
+
+    :param model: The model identifier or path on Hugging Face.
+    :param token: The Hugging Face API token.
+    :return: A tuple containing the tokenizer, model, and the text-generation pipeline.
+    """
+
+    # Set quantization configuration to load the llama model with less GPU memory
+    bnb_config = transformers.BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model, token=token)
+
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        quantization_config=bnb_config,
+        token=token,
+    )
+
+    query_pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.float16,
+        max_new_tokens=150,
+        truncation=True,
+        device_map="auto",
+    )
+
+    return tokenizer, model, query_pipeline
+
+
+def load_documents_and_split(pdf_path: str, chunk_size: int = 1000) -> List[Document]:
+    """
+    Load a PDF document, split it into chunks, and return the vector store.
+
+    :param pdf_path: Path to the PDF file.
+    :param chunk_size: Size of the chunk to split the document.
+    :return: List of split documents.
+    """
+
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=100
+    )
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2",
+        model_kwargs={"device": "cuda"},
+    )
+
+    return Chroma.from_documents(
+        documents=text_splitter.split_documents(documents),
+        embedding=embeddings,
+        collection_metadata={"hnsw:space": "cosine"},
+    )
+
+
+def custom_retriever(query: str, vectordb) -> List[Document]:
+    """
+    Custom retriever to fetch documents based on the query.
+
+    :param query: Query string from the user.
+    :param vectordb: The vector store to search in.
+    :return: Filtered list of relevant documents.
+    """
+
+    docs, scores = zip(*vectordb.similarity_search_with_score(query, k=20))
+
+    for doc, score in zip(docs, scores):
+        doc.metadata["score"] = round(score, 2)
+
+    filtered_results = [doc for doc in docs if (1 - doc.metadata["score"]) >= 0.4]
+    return list(filtered_results)
